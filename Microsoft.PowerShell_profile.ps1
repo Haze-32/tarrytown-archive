7630e1706052945da89ea064686255a7dcdd4caa
Haze <william.klus@gmail.com>
2026-01-01 11:41:48 -0500

    deploy

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,7 +0,0 @@
-function deploy {
-  git add .
-  git commit -m "deploy"
-  git push origin main
-  git push live main
-  ssh battley4@battlestationbuilds.com "/home1/battley4/repositories/tarrytown-archive/deploy.sh"
-}
